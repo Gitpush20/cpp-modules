2998e7565b16ada97ab9365f7ed68eb794448c86
Mohammed Bel bassi <mbel-bas@e1r1p9.1337.ma>
2022-05-16 14:08:55 +0100

    ...

---
+++ cpp_modules/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbel-bas <mbel-bas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/03 17:59:47 by mbel-bas          #+#    #+#             */
+/*   Updated: 2022/04/03 18:14:59 by mbel-bas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <locale>
+
+int  main(int argc, char const *argv[])
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string str(argv[i]);
+			for (size_t j = 0; j < str.length(); j++)
+				std::cout << (char)std::toupper(str[j]);
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
---
+++ cpp_modules/ex01/contact.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbel-bas <mbel-bas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/03 18:24:00 by mbel-bas          #+#    #+#             */
+/*   Updated: 2022/04/03 18:26:54 by mbel-bas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+class contact
+{
+private:
+    
+public:
+    contact(/* args */);
+    ~contact();
+};
+
+contact::contact(/* args */)
+{
+}
+
+contact::~contact()
+{
+}
+
+#endif
